2d601fd73e581a80bcd3e2335b96bc2e99b8644f
cincenfrank <fpetrangeli1@gmail.com>
2021-10-27 18:13:48 +0200

    work in progress backup

---
+++ bonus/js/script.js
@@ -0,0 +1,208 @@
+let numbersList = [];
+let firstRow;
+/**
+ * @type {HTMLElement}
+ */
+let secondRow;
+let thirdRow;
+let firstTitle;
+let duration;
+function onDomLoaded() {
+  const newGameBtn = document.getElementById("new-game");
+  numbersList = generateNumbersList();
+  newGameBtn.addEventListener("click", () => {
+    generateSimonNumbersHTML(numbersList);
+    generateReadyButton();
+  });
+
+  //   setTimeout(() => {
+  //     let userSaid = prompt(
+  //       "Insert the numbers previously said by Simon. You can separate different numbers with commas. We will consider only the first 5 numbers"
+  //     );
+  //     if (userSaid) {
+  //       let userSaidArray = userSaid
+  //         .split(",", 5)
+  //         .reduce((accumulator, currentValue) => {
+  //           const intValue = parseInt(currentValue);
+  //           if (!accumulator.includes(intValue)) {
+  //             accumulator.push(intValue);
+  //           }
+  //           return accumulator;
+  //         }, []);
+  //       let correctArray = checkScore(userSaidArray, numbersList);
+  //       let message = `You scored ${correctArray.length}/${numbersList.length}`;
+  //       if (correctArray.length > 0) {
+  //         message = `${message}
+  // You remembered the following numbers:
+  // ${correctArray.toString()}`;
+  //       }
+  //       alert(message);
+  //     }
+  //     // .map((element) => parseInt(element));
+  //   }, 30000);
+  // console.log("Timer Started");
+}
+
+/**
+ *
+ * @param {[number]} numbersList
+ * @param {HTMLElement} htmlElement
+ */
+function generateSimonNumbersHTML(numbersList) {
+  firstRow = document.getElementById("first-row");
+  thirdRow = document.getElementById("third-row");
+  firstTitle = document.getElementById("first-title");
+
+  let htmlText = "";
+  for (let i = 0; i < numbersList.length; i++) {
+    const number = numbersList[i];
+    htmlText += `<div class="col">
+                    <div class="card p-5 align-items-center fs-2 shadow">
+                        ${number}
+                    </div>
+                </div>`;
+  }
+  firstRow.innerHTML = htmlText;
+  firstTitle.innerHTML = "Simon said:";
+}
+
+function generateReadyButton() {
+  const col = document.createElement("div");
+  col.classList.add("col", "text-center");
+  const btn = document.createElement("div");
+  btn.innerText = "I'm Ready!";
+  btn.classList.add("btn", "btn-info");
+  btn.addEventListener("click", startCountdown);
+  col.appendChild(btn);
+  thirdRow.appendChild(col);
+}
+
+function startCountdown(params) {
+  console.log("start countdown");
+
+  let htmlText = "";
+  for (let i = 0; i < numbersList.length; i++) {
+    const number = numbersList[i];
+    htmlText += `<div class="col">
+                    <div class="card p-5 align-items-center fs-2 shadow">
+                        ???
+                    </div>
+                </div>`;
+  }
+  firstRow.innerHTML = htmlText;
+  firstTitle.innerHTML = "What did Simon say??";
+
+  secondRow = document.getElementById("second-row");
+  const col = document.createElement("div");
+  col.classList.add("col", "text-center");
+  const count = document.createElement("div");
+  count.innerText = "30";
+  count.classList.add("fs-1");
+  count.addEventListener("click", startCountdown);
+  col.appendChild(count);
+  secondRow.appendChild(col);
+  duration = 30;
+  setTimeout(() => {
+    timeoutFunc(count);
+  }, 1000);
+}
+
+function showUserInput() {
+  let htmlText = "";
+  for (let i = 0; i < numbersList.length; i++) {
+    htmlText += `<div class="col">
+    <div class="card p-5 align-items-center fs-2 shadow">
+        <input type="number" class=" w-100">
+    </div>
+</div>`;
+  }
+  secondRow.innerHTML = htmlText;
+  const col = document.createElement("div");
+  col.classList.add("col", "text-center");
+  const btn = document.createElement("div");
+  btn.innerText = "confirm numbers";
+  btn.classList.add("btn", "btn-info");
+  btn.addEventListener("click", onInputConfirmed);
+  col.appendChild(btn);
+  thirdRow.appendChild(col);
+}
+
+function onInputConfirmed(params) {
+  console.log("input confirmed");
+  generateSimonNumbersHTML(numbersList);
+  thirdRow.innerHTML = "";
+  let colInputs = secondRow.children;
+  for (let i = 0; i < colInputs.length; i++) {
+    const element = colInputs.item(i).firstChild;
+
+    console.log(element.firstChild.value);
+  }
+}
+
+function timeoutFunc(count) {
+  duration--;
+  count.textContent = duration.toString();
+  if (duration <= 0) {
+    console.log("timer end");
+    showUserInput();
+  } else {
+    setTimeout(() => {
+      timeoutFunc(count);
+    }, 1000);
+  }
+}
+
+/**
+ * Check input arrays values and returns an array only composed by matching values
+ * @param {[number]} userSaidArray
+ * @param {[number]} numbersArray
+ * @returns {[number]}
+ */
+function checkScore(userSaidArray, numbersArray) {
+  // let correctNumbersArray = [];
+  // for (let i = 0; i < userSaidArray.length; i++) {
+  //   const element = userSaidArray[i];
+  //   if (numbersArray.includes(element)) {
+  //     correctNumbersArray.push(element);
+  //   }
+  // }
+  let correctNumbersArray = numbersArray.reduce((accumulator, value) => {
+    if (userSaidArray.includes(value)) {
+      accumulator.push(value);
+    }
+    return accumulator;
+  }, []);
+  return correctNumbersArray;
+}
+
+/**
+ * Generates an array of random integer
+ * @param {number} arraySize length of the array to be returned
+ * @param {number} min minimum value (inclusive)
+ * @param {number} max maximum value (exclusive)
+ * @returns {[number]}
+ */
+function generateNumbersList(arraySize = 5, min = 0, max = 1000) {
+  const newList = [];
+  while (newList.length < arraySize) {
+    const randomNumber = getRandomInt(min, max);
+    if (!newList.includes(randomNumber)) {
+      newList.push(randomNumber);
+    }
+  }
+  return newList;
+}
+
+/**
+ * Generates a random integer between given numbers.
+ * @param {number} min minimum value (inclusive)
+ * @param {number} max maximum value (exclusive)
+ * @returns {number}
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+window.addEventListener("load", onDomLoaded);
