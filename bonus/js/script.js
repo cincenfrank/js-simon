3236039239cf224b1bcdf4ca16054537526fd489
cincenfrank <fpetrangeli1@gmail.com>
2021-10-27 21:15:18 +0200

    basic came logic and layout pre refactoring

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,11 +7,12 @@ let secondRow;
 let thirdRow;
 let firstTitle;
 let duration;
+let timeoutId;
 function onDomLoaded() {
   const newGameBtn = document.getElementById("new-game");
-  numbersList = generateNumbersList();
+
   newGameBtn.addEventListener("click", () => {
-    generateSimonNumbersHTML(numbersList);
+    generateSimonNumbersHTML();
     generateReadyButton();
   });
 
@@ -48,9 +49,17 @@ function onDomLoaded() {
  * @param {[number]} numbersList
  * @param {HTMLElement} htmlElement
  */
-function generateSimonNumbersHTML(numbersList) {
+function generateSimonNumbersHTML() {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+    timeoutId = undefined;
+  }
+  numbersList = generateNumbersList();
   firstRow = document.getElementById("first-row");
+  secondRow = document.getElementById("second-row");
   thirdRow = document.getElementById("third-row");
+  secondRow.innerHTML = "";
+  thirdRow.innerHTML = "";
   firstTitle = document.getElementById("first-title");
 
   let htmlText = "";
@@ -74,12 +83,13 @@ function generateReadyButton() {
   btn.classList.add("btn", "btn-info");
   btn.addEventListener("click", startCountdown);
   col.appendChild(btn);
+  thirdRow.innerHTML = "";
   thirdRow.appendChild(col);
 }
 
-function startCountdown(params) {
+function startCountdown() {
   console.log("start countdown");
-
+  thirdRow.innerHTML = "";
   let htmlText = "";
   for (let i = 0; i < numbersList.length; i++) {
     const number = numbersList[i];
@@ -92,7 +102,6 @@ function startCountdown(params) {
   firstRow.innerHTML = htmlText;
   firstTitle.innerHTML = "What did Simon say??";
 
-  secondRow = document.getElementById("second-row");
   const col = document.createElement("div");
   col.classList.add("col", "text-center");
   const count = document.createElement("div");
@@ -112,7 +121,7 @@ function showUserInput() {
   for (let i = 0; i < numbersList.length; i++) {
     htmlText += `<div class="col">
     <div class="card p-5 align-items-center fs-2 shadow">
-        <input type="number" class=" w-100">
+        <input type="number" class=" w-100 text-center">
     </div>
 </div>`;
   }
@@ -124,19 +133,32 @@ function showUserInput() {
   btn.classList.add("btn", "btn-info");
   btn.addEventListener("click", onInputConfirmed);
   col.appendChild(btn);
+  thirdRow.innerHTML = "";
   thirdRow.appendChild(col);
 }
 
 function onInputConfirmed(params) {
   console.log("input confirmed");
-  generateSimonNumbersHTML(numbersList);
+  // generateSimonNumbersHTML(numbersList);
   thirdRow.innerHTML = "";
-  let colInputs = secondRow.children;
-  for (let i = 0; i < colInputs.length; i++) {
-    const element = colInputs.item(i).firstChild;
+  let inputsList = document.querySelectorAll("input");
+  let userNumbers = [];
+  for (let i = 0; i < inputsList.length; i++) {
+    const elementValue = parseInt(inputsList.item(i).value);
 
-    console.log(element.firstChild.value);
+    if (!userNumbers.includes(elementValue)) {
+      userNumbers.push(elementValue);
+    }
   }
+  let scoreResult = checkScore(userNumbers, numbersList);
+  let message = `You scored ${scoreResult.length}/${numbersList.length}`;
+  if (scoreResult.length > 0) {
+    message = `${message}
+  You remembered the following numbers:
+  ${scoreResult.toString()}`;
+  }
+  // secondRow.innerHTML = "";
+  thirdRow.innerHTML = `<h2 class="text-center">${message}</h2>`;
 }
 
 function timeoutFunc(count) {
@@ -204,5 +226,4 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
-
 window.addEventListener("load", onDomLoaded);
